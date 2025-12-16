8b0ecae166a6328de1cd1dfb228a444f8cc73ef3
ChaHyunWoo <2471005@hansung.ac.kr>
2025-12-16 19:24:46 +0900

    Chapter2 예제 실습 완료

--- src/main/java/me/chahyunwoo/springbootdeveloper/TestController.java
+++ src/main/java/me/chahyunwoo/springbootdeveloper/TestController.java
@@ -1,4 +1,12 @@
 package me.chahyunwoo.springbootdeveloper;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+    @GetMapping("/test")
+    public String test(){
+        return "Hello, world!";
+    }
 }
