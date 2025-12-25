b5b5a3cd116159bd5a1b861e75408d6d0b0b3d57
ChaHyunWoo <2471005@hansung.ac.kr>
2025-12-25 21:42:08 +0900

    블로그 글 목록 뷰 구현

---
+++ src/main/java/me/chahyunwoo/springbootdeveloper/dto/ArticleListViewResponse.java
@@ -0,0 +1,17 @@
+package me.chahyunwoo.springbootdeveloper.dto;
+
+import lombok.Getter;
+import me.chahyunwoo.springbootdeveloper.domain.Article;
+
+@Getter
+public class ArticleListViewResponse {
+    private final Long id;
+    private final String title;
+    private final String content;
+    public ArticleListViewResponse(Article article){
+        this.id=article.getId();
+        this.title=article.getTitle();
+        this.content=article.getContent();
+    }
+
+}
