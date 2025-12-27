aa5a228f63b5344bb59ed87267742e7aefef3e43
ChaHyunWoo <2471005@hansung.ac.kr>
2025-12-27 10:29:45 +0900

    회원 가입 기능 구현 (Controller, Service, DTO)

---
+++ src/main/java/me/chahyunwoo/springbootdeveloper/service/UserService.java
@@ -0,0 +1,24 @@
+package me.chahyunwoo.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.chahyunwoo.springbootdeveloper.domain.User;
+import me.chahyunwoo.springbootdeveloper.dto.AddUserRequest;
+import me.chahyunwoo.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto){
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+        // 패스워드 암호화
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
