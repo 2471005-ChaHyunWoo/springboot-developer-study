b4e3f8074b0b540a233c540e05b4a03d9cdd9925
ChaHyunWoo <2471005@hansung.ac.kr>
2025-12-29 12:16:20 +0900

    토큰 재발급을 위한 API 컨트롤러 구현 및 DTO 수정

---
+++ src/main/java/me/chahyunwoo/springbootdeveloper/dto/CreateAccessTokenRequest.java
@@ -0,0 +1,10 @@
+package me.chahyunwoo.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateAccessTokenRequest {
+    private String refreshToken;
+}
